390a79ca79a5702c18da75109e0e1522fd2d83f8
Ratan <008ratan@gmail.com>
2021-10-24 20:10:44 +0530

    some chnages

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -33,8 +33,7 @@ export class PageComponent implements OnInit {
   }
 
   deleteSelectedTodos() {
-    //need ES5 to reverse loop in order to splice by index
-    for(var i=(this.todos.length -1); i > -1; i--) {
+    for(let i=(this.todos.length -1); i > -1; i--) {
       if(this.todos[i].completed) {
         this.todos.splice(i, 1);
       }
