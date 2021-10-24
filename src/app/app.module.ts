cc868be03ddcc87d3eab6472abf66ee50058f551
Ratan <008ratan@gmail.com>
2021-10-24 20:01:01 +0530

    todo App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PageComponent } from './page/page.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatListModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.css']
+})
+export class PageComponent implements OnInit {
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  addTodo(event: { preventDefault: () => void; }) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index: any) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteSelectedTodos() {
+    //need ES5 to reverse loop in order to splice by index
+    for(var i=(this.todos.length -1); i > -1; i--) {
+      if(this.todos[i].completed) {
+        this.todos.splice(i, 1);
+      }
+    }
+  }
+
+}
